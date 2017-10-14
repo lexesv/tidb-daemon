bbe22b70820bfcd89ecd6b6d7d02ad3fec85e252
Alex Vasilega <lexesv@gmail.com>
2017-10-14 16:55:00 +0300

    Update daemon.go

--- daemon.go
+++ daemon.go
@@ -24,12 +24,11 @@ func Daemon() {
 	if *daemon {
 		var err error
 		var cmd *exec.Cmd
-
 		Self, err := os.Executable()
 		if err != nil {
 			panic(err)
 		}
-		os.Args = DeleteInSlice(Self, os.Args)
+		os.Args = DeleteKeyInSlice(0, os.Args)
 		os.Args = DeleteInSlice("-d", os.Args)
 		cmd = exec.Command(Self, os.Args...)
 		if err = cmd.Start(); err != nil {
@@ -44,6 +43,16 @@ func DeleteInSlice(str string, list []string) []string {
 	var newlist []string
 	for _, s := range list {
 		if s != str {
+			newlist = append(newlist, s)
+		}
+	}
+	return newlist
+}
+
+func DeleteKeyInSlice(key int, list []string) []string {
+	var newlist []string
+	for i, s := range list {
+		if i != key {
 			newlist = append(newlist, s)
 		}
 	}
